a9ec066c2a5e420b3639fc9daea61dc974ff1e66
josuh <josuhan1433@gmail.com>
2022-07-21 20:57:52 +0900

    Changes

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,11 +1,14 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.Answer.AnswerForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -15,21 +18,50 @@ public class QuestionController {
     @Autowired
     private final QuestionService questionService;
 
+
     public QuestionController(QuestionService questionService) {
         this.questionService = questionService;
     }
 
-    @RequestMapping("/question/list")
-    public String list(Model model) {
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList", questionList);
-        return "question_list";
-    }
+//    @RequestMapping("/question/list")
+//    public String list(Model model) {
+//        List<Question> questionList = this.questionService.getList();
+//        model.addAttribute("questionList", questionList);
+//        return "question_list";
+//    }
 
     @RequestMapping(value = "/question/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/question/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+//    @PostMapping("/question/create")
+//    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+//        // TODO 질문을 저장한다.
+//        this.questionService.create(subject, content);
+//        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
+//    }
+
+    @PostMapping("/question/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
+    }
+
+    @RequestMapping("/question/list")
+    public String list(Model model, @RequestParam(value="page", defaultValue = "0") int page){
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
+        return "question_list";
     }
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -11,6 +11,7 @@ import com.mysite.sbb.Answer.Answer;
 import com.mysite.sbb.Answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,24 +22,15 @@ import org.springframework.transaction.annotation.Transactional;
 class SbbApplicationTests {
 
 	@Autowired
-	private AnswerRepository answerRepository;
-
-	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 
 
-	@Transactional
 	@Test
-	void testJpa() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		List<Answer> answerList = q.getAnswerList();
-
-		assertEquals(1,answerList.size());
-		assertEquals("네 자동으로 생성됩니다" ,answerList.get(0).getContent());
-
-
+	void testJpa(){
+		for(int i = 1; i<=300; i++){
+			String subject = String.format("테스트 데이터입니다: [%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject,content);
+		}
 	}
 }
